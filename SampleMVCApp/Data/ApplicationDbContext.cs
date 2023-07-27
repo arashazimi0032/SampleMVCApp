16c31afb5f1141b3ecd5cb7bae1f90425c5eb878
arashazimi0032 <arashazimi0032@gmail.com>
2023-07-27 20:29:58 +0330

    connect to a server add applicationdbcontext and category model and create a migration

---
+++ SampleMVCApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleMVCApp.Models;
+
+namespace SampleMVCApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
--- SampleMVCApp/Program.cs
+++ SampleMVCApp/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SampleMVCApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(o => 
+o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
