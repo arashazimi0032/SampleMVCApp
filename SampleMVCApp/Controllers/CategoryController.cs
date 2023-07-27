81903991212c414b3753fed33a51f4a9adcbcab0
arashazimi0032 <arashazimi0032@gmail.com>
2023-07-27 20:58:42 +0330

    Seed Category Table Create View for Category Table

---
+++ SampleMVCApp/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleMVCApp.Data;
+using SampleMVCApp.Models;
+
+namespace SampleMVCApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
--- SampleMVCApp/Migrations/20230727171236_SeedToCategoryTable.Designer.cs
+++ SampleMVCApp/Migrations/20230727171236_SeedToCategoryTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SampleMVCApp.Data;
 
@@ -10,9 +11,11 @@ using SampleMVCApp.Data;
 namespace SampleMVCApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230727171236_SeedToCategoryTable")]
+    partial class SeedToCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
