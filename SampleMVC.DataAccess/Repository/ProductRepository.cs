418d37b4c2b7763297b8b5dc316e3757e6dc3147
arashazimi0032 <arashazimi0032@gmail.com>
2023-07-30 00:28:41 +0330

    Add Product Model . Add all CRUD operations for Product table and modify all Views for that.

---
+++ SampleMVC.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using SampleMVC.DataAccess.Repository.IRepository;
+using SampleMVC.Models;
+using SampleMVCApp.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMVC.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
---
+++ SampleMVCApp/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SampleMVC.DataAccess.Repository.IRepository;
+using SampleMVC.Models;
+using SampleMVCApp.DataAccess.Data;
+using System.Collections.Generic;
+
+namespace SampleMVCApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            return View(objProductList);
+        }
+
+        public IActionResult Create()
+        {
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll()
+                .Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                });
+            //ViewBag.CategoryList = CategoryList;  // Way 1 ViewBag Can Use Directly
+            ViewData["CategoryList"] = CategoryList;  // Way 2 ViewData not can use directly but need to cast as IEnumerable<SelectListItem>
+            //--!! Important!!--// ViewBag internally inserts data into ViewData dictionary. So the key of ViewData and property of ViewBag must Not match.
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index", "Product");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            //Product? productFromDb = _db.Categories.Find(id); // just for primary key
+            //Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id = id); // works for every things like (u => u.Name.Contain("Blah"))
+            //Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction("Index", "Product");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            //Product? productFromDb = _db.Categories.Find(id); // just for primary key
+            //Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id = id); // works for every things like (u => u.Name.Contain("Blah"))
+            //Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Product obj = _unitOfWork.Product.Get(u => u.Id == id);
+            if (obj == null) { return NotFound(); }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction("Index", "Product");
+        }
+    }
+}
