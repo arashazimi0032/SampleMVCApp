fc361c6d79b2b9f1ba212a92ee3dabbe0fa46697
arashazimi0032 <arashazimi0032@gmail.com>
2023-07-31 20:08:07 +0330

    Delete function converted to an API for product entity and a jquery function handle deleting process

--- SampleMVCApp/Areas/Admin/Controllers/ProductController.cs
+++ SampleMVCApp/Areas/Admin/Controllers/ProductController.cs
@@ -109,33 +109,33 @@ namespace SampleMVCApp.Areas.Admin.Controllers
             }
         }
 
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            //Product? productFromDb = _db.Categories.Find(id); // just for primary key
-            //Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id = id); // works for every things like (u => u.Name.Contain("Blah"))
-            //Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
+        //public IActionResult Delete(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+        //    //Product? productFromDb = _db.Categories.Find(id); // just for primary key
+        //    //Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id = id); // works for every things like (u => u.Name.Contain("Blah"))
+        //    //Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+        //    if (productFromDb == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(productFromDb);
+        //}
 
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePost(int? id)
-        {
-            Product obj = _unitOfWork.Product.Get(u => u.Id == id);
-            if (obj == null) { return NotFound(); }
-            _unitOfWork.Product.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "Product deleted successfully";
-            return RedirectToAction("Index", "Product");
-        }
+        //[HttpPost, ActionName("Delete")]
+        //public IActionResult DeletePost(int? id)
+        //{
+        //    Product obj = _unitOfWork.Product.Get(u => u.Id == id);
+        //    if (obj == null) { return NotFound(); }
+        //    _unitOfWork.Product.Remove(obj);
+        //    _unitOfWork.Save();
+        //    TempData["success"] = "Product deleted successfully";
+        //    return RedirectToAction("Index", "Product");
+        //}
 
         #region API CALLS
         [HttpGet]
@@ -143,6 +143,28 @@ namespace SampleMVCApp.Areas.Admin.Controllers
         {
             List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             return Json(new { data = objProductList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            Product productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting."});
+            }
+
+            string oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath,
+                productToBeDeleted.ImageUrl.TrimStart('\\'));
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.Product.Remove(productToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successfully." });
         }
         #endregion
     }
