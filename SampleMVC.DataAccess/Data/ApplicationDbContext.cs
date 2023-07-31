de41bfaecc70a2ee58ea1073d1fd98bcd850209d
arashazimi0032 <arashazimi0032@gmail.com>
2023-08-01 00:11:18 +0330

    add IdentityFrameWork to project.

--- SampleMVC.DataAccess/Data/ApplicationDbContext.cs
+++ SampleMVC.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SampleMVC.Models;
 
 namespace SampleMVCApp.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -14,6 +16,9 @@ namespace SampleMVCApp.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            
+            base.OnModelCreating(modelBuilder); // this is need when using IdentityDbContext
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
