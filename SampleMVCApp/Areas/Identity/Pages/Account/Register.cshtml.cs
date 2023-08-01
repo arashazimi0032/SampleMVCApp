ab903973b0d138ccd05ffcfc12e18134ea87e73e
arashazimi0032 <arashazimi0032@gmail.com>
2023-08-01 23:30:31 +0330

    set ApplicationUser as Default registration user

--- SampleMVCApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SampleMVCApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using SampleMVC.Models;
 
 namespace SampleMVCApp.Areas.Identity.Pages.Account
 {
@@ -154,11 +155,11 @@ namespace SampleMVCApp.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
