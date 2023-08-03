e065bfc76deba34236aa0d6928b2de44c7e7cdab
arashazimi0032 <arashazimi0032@gmail.com>
2023-08-03 15:37:46 +0330

    create orderHeader and orderDetail and complete repositories and UnitOfWork for that.

---
+++ SampleMVC.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,26 @@
+﻿using SampleMVC.DataAccess.Repository.IRepository;
+using SampleMVC.Models;
+using SampleMVCApp.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMVC.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OrderDetailRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetails.Update(obj);
+        }
+    }
+}
