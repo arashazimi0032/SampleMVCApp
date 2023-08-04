eb0de2cdf8be8d9358707af50bdd649b7ee81a57
arashazimi0032 <arashazimi0032@gmail.com>
2023-08-04 18:11:00 +0330

    Order Management: add orderController and orderViewMode

---
+++ SampleMVCApp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleMVC.DataAccess.Repository.IRepository;
+using SampleMVC.Models;
+
+namespace SampleMVCApp.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public OrderController(IUnitOfWork unitOfWork)
+        {
+			_unitOfWork = unitOfWork;
+		}
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data = objOrderHeader });
+		}
+		#endregion
+	}
+}
