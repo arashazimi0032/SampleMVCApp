9e6b12baf51e10fe5743f1f0d1943d8ca8f7e5be
arashazimi0032 <arashazimi0032@gmail.com>
2023-08-04 19:01:27 +0330

    #order management: code behind fiter orders based on payment status

--- SampleMVCApp/Areas/Admin/Controllers/OrderController.cs
+++ SampleMVCApp/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SampleMVC.DataAccess.Repository.IRepository;
 using SampleMVC.Models;
+using SampleMVC.Utility;
+using System.Diagnostics;
 
 namespace SampleMVCApp.Areas.Admin.Controllers
 {
@@ -20,9 +22,29 @@ namespace SampleMVCApp.Areas.Admin.Controllers
 
 		#region API CALLS
 		[HttpGet]
-		public IActionResult GetAll()
+		public IActionResult GetAll(string status)
 		{
-			List<OrderHeader> objOrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			IEnumerable<OrderHeader> objOrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+			switch (status)
+			{
+
+                case "pending":
+                    objOrderHeader = objOrderHeader.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeader = objOrderHeader.Where(u => u.PaymentStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeader = objOrderHeader.Where(u => u.PaymentStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeader = objOrderHeader.Where(u => u.PaymentStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
 			return Json(new { data = objOrderHeader });
 		}
 		#endregion
