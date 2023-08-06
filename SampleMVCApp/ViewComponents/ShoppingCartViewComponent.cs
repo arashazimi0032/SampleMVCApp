e60eadea48db92840affd9128a5a353b7c522b43
arashazimi0032 <arashazimi0032@gmail.com>
2023-08-06 21:00:52 +0330

    Add ShoppingCartViewComponent model for get and set ShoppingCart Count.

---
+++ SampleMVCApp/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleMVC.DataAccess.Repository.IRepository;
+using SampleMVC.Utility;
+using System.Security.Claims;
+
+namespace SampleMVCApp.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (userId != null)
+            {
+                if (HttpContext.Session.GetInt32(SD.SessionCart) == null)
+                {
+                    HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
+                    .GetAll(u => u.ApplicationUserId == userId.Value).Count());
+                }
+                
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
+    }
+}
