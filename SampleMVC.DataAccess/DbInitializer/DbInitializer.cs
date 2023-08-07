412d8b3b88f49c9bd44a4911fb3b5a5bf3a92155
arashazimi0032 <arashazimi0032@gmail.com>
2023-08-07 21:52:11 +0330

    create DbInitializer for initialize roles and create an initial admin and ...

---
+++ SampleMVC.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using SampleMVC.DataAccess.DbInitializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMVC.DataAccess.DBInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
