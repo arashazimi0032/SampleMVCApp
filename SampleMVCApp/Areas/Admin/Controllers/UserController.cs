38b47f40453e44f30bf2460cde06d392ec7f7863
arashazimi0032 <arashazimi0032@gmail.com>
2023-08-11 03:06:56 +0330

    # user management display user list

---
+++ SampleMVCApp/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SampleMVC.DataAccess.Repository.IRepository;
+using SampleMVC.Models;
+using SampleMVC.Models.ViewModels;
+using SampleMVC.Utility;
+using SampleMVCApp.DataAccess.Data;
+using System.Collections.Generic;
+
+namespace SampleMVCApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+            return Json(new { data = objUserList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            return Json(new { success = true, message = "Delete Successfully." });
+        }
+        #endregion
+    }
+}
