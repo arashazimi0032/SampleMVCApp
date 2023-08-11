a1a5971f11a5fecb3420424f5f5e8b52d810d5f8
arashazimi0032 <arashazimi0032@gmail.com>
2023-08-11 22:39:15 +0330

    show all images of a product in edit page of that

--- SampleMVCApp/Areas/Admin/Controllers/ProductController.cs
+++ SampleMVCApp/Areas/Admin/Controllers/ProductController.cs
@@ -53,7 +53,7 @@ namespace SampleMVCApp.Areas.Admin.Controllers
             else
             {
                 //update 
-                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id, includeProperties:"ProductImages");
                 return View(productVM);
             }
         }
